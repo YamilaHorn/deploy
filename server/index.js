b581480f24083c67464faa8ca8bd414c6a59627a
YamilaHorn <hornmacarena12@hotmail.com>
2023-11-02 14:09:49 -0300

    deploy

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const ApiToDb = require('./src/handlers/ApiToDb')
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 require('dotenv').config();
+const { PORT } = process.env;
 
 conn.sync({ force: true })
 .then(() => {
-server.listen(process.env.PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening at`, process.env.PORT);
   ApiToDb()
 })
